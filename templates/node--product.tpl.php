79099ca33fbe3c31bf29dc98387bfa07396ce2f6
Vandenborne Stefan <stefan@illutek.be>
2016-05-19 16:17:38 +0200

    melding ivm merken op node toegevoegd en prijzen tonen en request bug newprice enkel dan werdt on request getoond

--- templates/node--product.tpl.php
+++ templates/node--product.tpl.php
@@ -7,116 +7,147 @@
  */
 if ($teaser): ?>
 
-    <div class="product_wrapper_teaser">
-        <div class="product_list">
-            <a href="<?php print $node_url; ?>">
-                <div class="item">
-                    <div class="teaser_prod_img">
-                        <?php print (isset($uc_image_teaser) ? $uc_image_teaser : ''); ?>
-                        <?php // print render($content['uc_product_image'][0]); ?>
-                    </div>
-                    <div class="body_teaser"><?php print $title; ?></div>
-                    <?php
-                    /**
-                     *
-                     */
-                    // print (isset($uc_sellPrice) ? $uc_sellPrice : '');
-                    $fieldUsedPrice = field_get_items('node', $node, 'field_secondhand_price');
-                    if ($fieldUsedPrice) {
-                        print render($content['field_secondhand_price']);
-                    } else {
-                        print t("Price on request");
-                    }
+  <div class="product_wrapper_teaser">
+    <div class="product_list">
+      <a href="<?php print $node_url; ?>">
+        <div class="item">
+          <div class="teaser_prod_img">
+            <?php print (isset($uc_image_teaser) ? $uc_image_teaser : ''); ?>
+            <?php // print render($content['uc_product_image'][0]); ?>
+          </div>
+          <div class="body_teaser"><?php print $title; ?></div>
+          <?php
+          /**
+           *
+           */
+          // print (isset($uc_sellPrice) ? $uc_sellPrice : '');
+          $fieldUsedPrice = field_get_items('node', $node, 'field_secondhand_price');
+          if ($fieldUsedPrice) {
+            print render($content['field_secondhand_price']);
+          } else {
+            print t("Price on request");
+          }
 
-                    /**
-                     * variables van template.php
-                     */
-                    print $details;
-                    ?>
-                </div>
-            </a>
+          /**
+           * variables van template.php
+           */
+          print $details;
+          ?>
+        </div>
+      </a>
+      <?php
+      /**
+       * variables van template.php
+       * add to cart buiten de a href anders is deze
+       * gewoon een link naar de node van het product
+       */
+      print $uc_addCart;
+      ?>
+
+    </div>
+  </div>
+
+<?php else: ?>
+  <div class="product_list">
+    <?php print render($title_prefix);
+    if (!$page): ?>
+      <h2 class="title" <?php print $title_attributes; ?>>
+        <a href="<?php print $node_url; ?>"><?php print $title; ?></a>
+      </h2>
+    <?php endif;
+    print render($title_suffix); ?>
+
+    <div class="content"<?php print $content_attributes; ?>>
+      <div class="product_wrapper">
+        <?php
+        /**
+         * Als de checkbox Telefunken aangevinkt is dan de brand_message
+         * niet tonen
+         */
+        if (!empty($content['field_telefunken']['#items'][0]['value']) == '1') {
+          print ("");
+        } else { ?>
+          <div class="col-md-12 brand_message orderMessageBlock">
             <?php
+            print t("Prices shown are for all models except for the following brands, Telefunken, Amperex,
+                    Philips, Mullard, Birmar Tung-Sol and Osram. For these brands is the price on request.");
+            ?>
+          </div>
+        <?php } ?>
+
+        <div class="row">
+          <div class="col-md-4 product_img">
+            <?php
+            print (isset($uc_image_teaser) ? $uc_image_teaser : '');
+            print (isset($uc_image) ? $uc_image : '');
+            ?>
+          </div>
+          <div class="col-md-8 product_info">
+            <div class="sku">
+              <?php print t("Tube: "); ?><?php print_r($model) ?>
+            </div>
+
+
+            <div class="sell-price">
+              <?php // print (isset($uc_sellPrice) ? $uc_sellPrice : ''); ?>
+
+              <?php
+              $fieldNewPrice = field_get_items('node', $node, 'field_new_price');
+              $fieldUsedPrice = field_get_items('node', $node, 'field_secondhand_price');
+              ?>
+
+              <?php
+              if ($fieldNewPrice && $fieldUsedPrice) { ?>
+                <div class="sell-price__price">
+                  <?php
+                  print render($content['field_new_price']);
+                  ?>
+                </div>
+                <div class="sell-price__price">
+                  <?php print render($content['field_secondhand_price']);
+                  ?>
+                </div>
+              <?php
+              } elseif ($fieldNewPrice) { ?>
+                <div class="sell-price__price">
+                  <?php
+                  print render($content['field_new_price']);
+                  ?>
+                </div>
+              <?php } elseif ($fieldUsedPrice) { ?>
+                <div class="sell-price__price">
+                  <?php
+                  print render($content['field_secondhand_price']);
+                  ?>
+                </div>
+              <?php } else { ?>
+                <div class="sell-price__price">
+                  <?php print t('Price on request'); ?>
+                </div>
+              <?php } ?>
+            </div>
+
+            <?php print (isset($uc_body) ? $uc_body : ''); ?>
+            <div class="row">
+              <?php
+              include 'partials/more-info-btn.inc.php';
+              ?>
+            </div>
+            <?php
+            print
+              '<div class=\'stock\'>' .
+              render($content['field_custom_stock']) .
+              '</div>';
             /**
-             * variables van template.php
-             * add to cart buiten de a href anders is deze
-             * gewoon een link naar de node van het product
+             * uc_addCart komt van template.php add_to_cart = quantity + btn to Cart
              */
             print $uc_addCart;
             ?>
 
+          </div>
         </div>
+      </div>
     </div>
-
-<?php else: ?>
-    <div class="product_list">
-        <?php print render($title_prefix);
-        if (!$page): ?>
-            <h2 class="title" <?php print $title_attributes; ?>>
-                <a href="<?php print $node_url; ?>"><?php print $title; ?></a>
-            </h2>
-        <?php endif;
-        print render($title_suffix); ?>
-
-        <div class="content"<?php print $content_attributes; ?>>
-            <div class="product_wrapper">
-                <div class="row">
-                    <div class="col-md-4 product_img">
-                        <?php
-                        print (isset($uc_image_teaser) ? $uc_image_teaser : '');
-                        print (isset($uc_image) ? $uc_image : '');
-                        ?>
-                    </div>
-                    <div class="col-md-8 product_info">
-                        <div class="sku">
-                            <?php print t("Tube: "); ?><?php print_r($model) ?>
-                        </div>
-
-
-                        <div class="sell-price">
-                            <?php // print (isset($uc_sellPrice) ? $uc_sellPrice : ''); ?>
-                            <div class="sell-price__price">
-                                <?php
-                                $fieldNewPrice = field_get_items('node', $node, 'field_new_price');
-                                if ($fieldNewPrice) {
-                                    print render($content['field_new_price']);
-                                } else {
-                                    print t("Price on request");
-                                }
-                                ?>
-                            </div>
-                            <?php
-                            if ($fieldNewPrice) { ?>
-                                <div class="sell-price__price">
-                                    <?php print render($content['field_secondhand_price']); ?>
-                                </div>
-                            <?php } else {
-                                print '';
-                            } ?>
-
-
-                        </div>
-
-                        <?php print (isset($uc_body) ? $uc_body : ''); ?>
-                        <div class="row">
-                            <?php
-                            include 'partials/more-info-btn.inc.php';
-                            ?>
-                        </div>
-                        <?php
-                        print
-                            '<div class=\'stock\'>' .
-                            render($content['field_custom_stock']) .
-                            '</div>';
-                        /**
-                         * uc_addCart komt van template.php add_to_cart = quantity + btn to Cart
-                         */
-                        print $uc_addCart;
-                        ?>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- end product_wrapper-->
-    </div>
+    <!-- end product_wrapper-->
+  </div>
 <?php endif; ?>
