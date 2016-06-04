28795b2fd0bd3dad3645c150270ae1c924251e40
Vandenborne Stefan <stefan@illutek.be>
2016-06-04 15:03:42 +0200

    teaser aanpassing daar ook de 2 prijzen tonen

--- templates/node--product.tpl.php
+++ templates/node--product.tpl.php
@@ -5,8 +5,14 @@
  * Date: 26/12/2015
  * Time: 14:00
  */
-if ($teaser): ?>
 
+/**
+ * variables aangemaakt op template.php newPrice, usedPrice en onRequest
+ */
+$fieldNewPrice = field_get_items('node', $node, 'field_new_price');
+$fieldUsedPrice = field_get_items('node', $node, 'field_secondhand_price');
+
+if ($teaser): ?>
   <div class="product_wrapper_teaser">
     <div class="product_list">
       <a href="<?php print $node_url; ?>">
@@ -21,11 +27,17 @@ if ($teaser): ?>
            *
            */
           // print (isset($uc_sellPrice) ? $uc_sellPrice : '');
-          $fieldUsedPrice = field_get_items('node', $node, 'field_secondhand_price');
-          if ($fieldUsedPrice) {
-            print render($content['field_secondhand_price']);
+
+
+          if ($fieldNewPrice && $fieldUsedPrice) {
+            print $newPrice;
+            print $usedPrice;
+          } elseif ($fieldNewPrice) {
+            print $newPrice;
+          } elseif ($fieldUsedPrice) {
+            print $usedPrice;
           } else {
-            print t("Price on request");
+            print $onRequest;
           }
 
           /**
@@ -49,53 +61,54 @@ if ($teaser): ?>
 
 <?php else: ?>
   <div class="product_list">
-    <?php print render($title_prefix);
-    if (!$page): ?>
-      <h2 class="title" <?php print $title_attributes; ?>>
-        <a href="<?php print $node_url; ?>"><?php print $title; ?></a>
-      </h2>
-    <?php endif;
-    print render($title_suffix); ?>
+  <?php print render($title_prefix);
+  if (!$page): ?>
+    <h2 class="title" <?php print $title_attributes; ?>>
+      <a href="<?php print $node_url; ?>"><?php print $title; ?></a>
+    </h2>
+  <?php endif;
+  print render($title_suffix); ?>
 
-    <div class="content"<?php print $content_attributes; ?>>
-      <div class="product_wrapper">
-        <?php
-        /**
-         * Als de checkbox Telefunken aangevinkt is dan de brand_message
-         * niet tonen
-         */
-        if (!empty($content['field_telefunken']['#items'][0]['value']) == '1') {
-          print ("");
-        } else { ?>
-          <div class="col-md-12 brand_message orderMessageBlock">
-            <?php
-            print t("Prices shown are for all models except for the following brands, Telefunken, Amperex,
+  <div class="content"<?php print $content_attributes; ?>>
+    <div class="product_wrapper">
+      <?php
+      /**
+       * Als de checkbox Telefunken aangevinkt is dan de brand_message
+       * niet tonen
+       */
+      if (!empty($content['field_telefunken']['#items'][0]['value']) == '1') {
+        print ("");
+      } else { ?>
+        <div class="col-md-12 brand_message orderMessageBlock">
+          <?php
+          print t("Prices shown are for all models except for the following brands, Telefunken, Amperex,
                     Philips, Mullard, Birmar Tung-Sol and Osram. For these brands is the price on request.");
-            ?>
-          </div>
-        <?php } ?>
+          ?>
+        </div>
+      <?php } ?>
 
-        <div class="row">
-          <div class="col-md-4 product_img">
+      <div class="row">
+        <div class="col-md-4 product_img">
+          <?php
+          print (isset($uc_image_teaser) ? $uc_image_teaser : '');
+          print '<div class="img_teaser_wrap">' . (isset($uc_image) ? $uc_image : '') . '</div>';
+          ?>
+        </div>
+        <div class="col-md-8 product_info">
+          <div class="product_info__sku">
             <?php
-            print (isset($uc_image_teaser) ? $uc_image_teaser : '');
-            print '<div class="img_teaser_wrap">' . (isset($uc_image) ? $uc_image : '') . '</div>';
-            ?>
-          </div>
-          <div class="col-md-8 product_info">
-            <div class="sku">
-              <?php print t("Tube: "); ?><?php print_r($model) ?>
-            </div>
+            /**
+             * Model van tube en merk (brand)
+             */
+            print t("Tube: "); ?><?php print_r($model);
 
+            print '<span class="product_info__brand">' . render($content['field_tags']) . '</span>';
+
+            print '</div>';
+            ?>
 
             <div class="sell-price">
               <?php
-              /**
-               * variables aangemaakt op template.php newPrice, usedPrice en onRequest
-               */
-              $fieldNewPrice = field_get_items('node', $node, 'field_new_price');
-              $fieldUsedPrice = field_get_items('node', $node, 'field_secondhand_price');
-
               if ($fieldNewPrice && $fieldUsedPrice) {
                 print $newPrice;
                 print $usedPrice;
@@ -115,13 +128,20 @@ if ($teaser): ?>
               ?>
             </div>
             <?php
-            print
-              '<div class=\'stock\'>' .
-              render($content['field_custom_stock']) .
-              '<br>'.
-              render($content['field_brands_amount']) .
-              '</div>';
+            print '<div class=\'stock\'>';
+            print render($content['field_custom_stock']);
 
+            global $user;
+            if (in_array('administrator', array_values($user->roles))) {
+              print render($content['field_brands_amount']);
+            } else {
+              print '';
+            }
+
+            print '</div>'; ?>
+
+
+            <?php
             /**
              * uc_addCart komt van template.php add_to_cart = quantity + btn to Cart
              */
